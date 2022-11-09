407d2ffe1eb83ebbcf48c3a0c873ec725b5b888c
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-09 13:26:32 -0600

    feat: Program prints out the name in the robot object

---
+++ src/Robot.java
@@ -0,0 +1,14 @@
+public class Robot {
+
+    //Instance variables (HAS A)
+    public String name;
+    public String weaponName;
+    public int weaponPower;
+
+    //Constructor
+    public Robot() {
+    this.name = "Robby";
+    this.weaponName = "Sword";
+    this.weaponPower = 50;
+    }
+}
