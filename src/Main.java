217ace4dd224680a002ea39d9a510e1785629181
Evan Voeltner <evanvoeltner@gmail.com>
2022-11-09 13:45:07 -0600

    feat: Weapon class can take in parameters for name and power and set them as instance variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Robot robot_one = new Robot();
+        Weapon sword = new Weapon("Sword", 50);
+        Weapon club = new Weapon("Club", 30);
+        Robot robot_one = new Robot(club);
         System.out.println(robot_one.name);
+        robot_one.Attack();
     }
 }
--- src/Robot.java
+++ src/Robot.java
@@ -2,13 +2,16 @@ public class Robot {
 
     //Instance variables (HAS A)
     public String name;
-    public String weaponName;
-    public int weaponPower;
+    public Weapon weapon;
 
     //Constructor
-    public Robot() {
+    public Robot(Weapon weapon) {
     this.name = "Robby";
-    this.weaponName = "Sword";
-    this.weaponPower = 50;
+    this.weapon = weapon;
+    }
+
+    //Methods (CAN DO)
+    public void Attack(){
+        System.out.println(this.name + " attacks with their " + this.weapon.name + " for " + this.weapon.power + " damage!");
     }
 }
